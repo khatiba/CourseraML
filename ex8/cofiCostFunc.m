6133e1c29948a88b18efd9efb66885fb57b193e1
Ahmad Khatib <ackhatib@gmail.com>
2018-08-06 09:13:01 -0700

    Finish assignment 8

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -40,20 +40,23 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+% size(X)
+% size(Theta)
+% size(R)
+% size(Y)
+
+M = ((X*Theta') - Y).*R;
 
 
+J = 1/2 * sum(sum(M.^2));
+J = J + (lambda/2)*sum(sum(Theta.^2));
+J = J + (lambda/2)*sum(sum(X.^2));
 
+X_grad = M * Theta;
+X_grad = X_grad + lambda*X;
 
-
-
-
-
-
-
-
-
-
-
+Theta_grad = (X' * M)';
+Theta_grad = Theta_grad + lambda*Theta;
 
 % =============================================================
 
